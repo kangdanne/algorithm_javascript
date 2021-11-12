a2ca769d816d3592a70d1b8e0d79c913e0183f58
kangdan <kangdanne@gmail.com>
2021-11-12 13:48:42 +0900

    Add: 프로그래머스 레벨01 - 07번

---
+++ programmers/level_01/07.js
@@ -0,0 +1,53 @@
+// 06. 평균 구하기
+
+/*
+ * 1-1. 입력된 수가 짝수라면 2로 나눕니다.
+ * 1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+ * 2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+ * 예를 들어, 입력된 수가 6이라면 6→3→10→5→16→8→4→2→1 이 되어 총 8번 만에 1이 됩니다.
+ * 위 작업을 몇 번이나 반복해야하는지 반환하는 함수, solution을 완성해 주세요.
+ * 단, 작업을 500번을 반복해도 1이 되지 않는다면 –1을 반환해 주세요.
+ */
+
+// my result - pass
+function solution(num) {
+  var answer = 0;
+  for (let i = 0; i < 500; i++) {
+    if (num !== 1) {
+      if (num % 2 == 0) {
+        num = num / 2;
+      } else {
+        num = num * 3 + 1;
+      }
+    } else {
+      return (answer = i);
+    }
+  }
+  return -1;
+}
+
+// my result refactoring
+// 중첩 if문을 삼항연산자로 수정
+function solution(num) {
+  var answer = 0;
+  for (let i = 0; i < 500; i++) {
+    if (num !== 1) {
+      num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+    } else {
+      return (answer = i);
+    }
+  }
+  return -1;
+}
+
+/**********************************
+ * anothers result
+ **********************************/
+function solution(num) {
+  var answer = 0;
+  while (num != 1 && answer != 500) {
+    num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+    answer++;
+  }
+  return num == 1 ? answer : -1;
+}
