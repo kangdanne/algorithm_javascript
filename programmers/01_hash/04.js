f5f95917719a649dcd16fcd1b2637e8d9143226b
kangdan <kangdanne@gmail.com>
2023-01-10 09:55:48 +0900

    04. 위장

---
+++ programmers/01_hash/04.js
@@ -0,0 +1,21 @@
+// 04. 위장
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/42578
+*/
+
+// my result - in progress
+function solution(clothes) {
+  var answer = 0;
+  const item = {};
+
+  for (i = 0; i < clothes.length; i++) {}
+
+  return answer;
+}
+
+solution([
+  ["yellow_hat", "headgear"],
+  ["blue_sunglasses", "eyewear"],
+  ["green_turban", "headgear"],
+]);
